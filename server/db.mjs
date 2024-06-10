0396004de5d9f4df1af8d70d39a3f3f475007a1d
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-06-10 13:36:54 +0200

    fix:db.mjs

--- server/db.mjs
+++ server/db.mjs
@@ -5,7 +5,7 @@ import sqlite3 from "sqlite3";
 // Opening the database
 // NOTE: to work properly you must run the server inside "server" folder (i.e., ./solution/server)
 try {
-    const db = new sqlite3.Database('DB/db.sqlite', (err) => {
+    const db = new sqlite3.Database('DB/db.db', (err) => {
         if (err) throw err;
     });
 } catch (err) {
