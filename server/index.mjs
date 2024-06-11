4f8f42757a4d2ba646d2a661a0a521bc7c229baf
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-06-11 10:43:57 +0200

    Fix:directory_needed_files

--- server/index.mjs
+++
@@ -1,8 +0,0 @@
-// server/index.mjs
-import express from 'express';
-import cors from 'cors';
-
-const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-
-// Other middleware and routes
