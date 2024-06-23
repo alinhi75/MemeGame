29b74684c194d6763a9dd10d15c4a6d5bf4fed05
alinhi75 <sayedali.noohi@studenti.polito.it>
2024-06-23 11:22:47 +0200

    fix:UserGameSummary&Profile

--- client/src/Components/ProfilePage.jsx
+++ client/src/Components/ProfilePage.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './CSS/ProfilePage.css';
 import { AuthContext } from '../AuthContext';
 
-const ProfilePage = ({isLoggedIn,handleLogout}) => {
+const ProfilePage = ({ isLoggedIn, handleLogout }) => {
   const { user, logout } = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
@@ -15,11 +15,6 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
   const [userInfo, setUserInfo] = useState(null);
   const [showLeaderboard, setShowLeaderboard] = useState(false);
   const [leaderboardData, setLeaderboardData] = useState([]);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(1);
- 
-  // check if user is logged in or not
 
   useEffect(() => {
     if (!username) {
@@ -51,25 +46,19 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
   // Function to calculate total score of all games
   const calculateTotalScore = (games) => {
     let total = 0;
-    games.forEach(game => {
+    games.forEach((game) => {
       total += game.score;
     });
     setTotalScore(total);
   };
-  // Function to handle logout
 
+  // Function to handle logout
   const handleLogoutClick = () => {
     handleLogout();
     navigate('/');
   };
-  // handle login Error
-
-  if (!username) {
-    return <p>Loading...</p>;
-  }
 
   // Function to fetch leaderboard data
-
   const fetchLeaderboardData = async () => {
     try {
       const response = await fetch('http://localhost:3001/api/leaderboard');
@@ -85,14 +74,14 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
   };
 
   // Function to handle play game
-
   const handlePlayGame = () => {
     navigate('/UserGame', { state: { username } });
   };
-  
+
   // Pagination logic
-  
+  const itemsPerPage = 1;
   const totalPages = Math.ceil(gameHistory.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const goToPage = (page) => {
     setCurrentPage(page);
@@ -114,7 +103,11 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
     <div className="profile-page-container container">
       {userInfo && (
         <div className="user-info text-center my-4">
-          <img src='../../public/ProfilePicture.png' alt="Profile" className="profile-picture img-thumbnail" />
+          <img
+            src="../../public/ProfilePicture.png"
+            alt="Profile"
+            className="profile-picture img-thumbnail"
+          />
           <h2>Welcome, {userInfo.username}</h2>
         </div>
       )}
@@ -128,11 +121,24 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
           currentGames.map((game, index) => (
             <div key={index} className="card game-history-item mb-3">
               <div className="card-body">
-                <h3 className="card-title">Game {indexOfFirstGame + index + 1} - Total Score: {game.score}</h3>
+                <h3 className="card-title">
+                  Game {Math.ceil(indexOfLastGame/3)} - Total Score: {game.score}
+                </h3>
                 <p>Round: {game.round}</p>
-                <img id='meme_image' src={game.image_path} alt="Game" className="game-image img-thumbnail" /><br /><br />
-                <p className={`caption-selected ${game.score === 0 ? 'incorrect' : ''}`}>Caption Selected: {game.caption}</p>
-                {game.score === 0 && <p id='correct_selected'>Correct Caption: {game.rightcaption}</p>}
+                <img
+                  id="meme_image"
+                  src={game.image_path}
+                  alt="Game"
+                  className="game-image img-thumbnail"
+                />
+                <br />
+                <br />
+                <p className={`caption-selected ${game.score === 0 ? 'incorrect' : ''}`}>
+                  Caption Selected: {game.caption}
+                </p>
+                {game.score === 0 && (
+                  <p id="correct_selected">Correct Caption: {game.rightcaption}</p>
+                )}
               </div>
             </div>
           ))
@@ -142,25 +148,44 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
         <nav aria-label="Page navigation example">
           <ul className="pagination justify-content-center">
             <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-              <button className="page-link" onClick={() => goToPreviousPage()} tabIndex="-1" aria-disabled="true">Previous</button>
+              <button
+                className="page-link"
+                onClick={() => goToPreviousPage()}
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                Previous
+              </button>
             </li>
             {Array.from({ length: totalPages }, (_, index) => (
               <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-                <button className="page-link" onClick={() => goToPage(index + 1)}>{index + 1}</button>
+                <button className="page-link" onClick={() => goToPage(index + 1)}>
+                  {index + 1}
+                </button>
               </li>
             ))}
             <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-              <button className="page-link" onClick={() => goToNextPage()}>Next</button>
+              <button className="page-link" onClick={() => goToNextPage()}>
+                Next
+              </button>
             </li>
           </ul>
         </nav>
       </div>
       <h3>Total Score of All Games: {totalScore}</h3>
       <div className="action-buttons text-center">
-        <button className="btn btn-primary m-2" onClick={handlePlayGame}>Play Game</button>
-        <button className="btn btn-secondary m-2" onClick={handleLogoutClick}>Logout</button>
-        <button className='btn btn-primary m-2' onClick={fetchLeaderboardData}>Leaderboard</button>
-        <button className='btn btn-primary m-2' onClick={() => navigate('/')}>Homepage</button>
+        <button className="btn btn-primary m-2" onClick={handlePlayGame}>
+          Play Game
+        </button>
+        <button className="btn btn-secondary m-2" onClick={handleLogoutClick}>
+          Logout
+        </button>
+        <button className="btn btn-primary m-2" onClick={fetchLeaderboardData}>
+          Leaderboard
+        </button>
+        <button className="btn btn-primary m-2" onClick={() => navigate('/')}>
+          Homepage
+        </button>
         {showLeaderboard && (
           <div className="leaderboard-table">
             <h2>Leaderboard</h2>
@@ -175,7 +200,6 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
               <tbody>
                 {leaderboardData.map((entry, index) => (
                   <tr key={index}>
-                    
                     <td>{index + 1}</td>
                     <td>{entry.username}</td>
                     <td>{entry.total_score}</td>
@@ -183,7 +207,9 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
                 ))}
               </tbody>
             </table>
-            <button className='btn btn-danger m-2' onClick={() => setShowLeaderboard(false)}>Cancel</button>
+            <button className="btn btn-danger m-2" onClick={() => setShowLeaderboard(false)}>
+              Cancel
+            </button>
           </div>
         )}
       </div>
@@ -192,4 +218,3 @@ const ProfilePage = ({isLoggedIn,handleLogout}) => {
 };
 
 export default ProfilePage;
-
--- client/src/Components/UserGame.jsx
+++ client/src/Components/UserGame.jsx
@@ -21,9 +21,11 @@ const UserGame = () => {
     const [usedMemes, setUsedMemes] = useState([]);
     const [messageType, setMessageType] = useState('info');
     const [captionClicks, setCaptionClicks] = useState(0);
+    const [gameSummary, setGameSummary] = useState([]);
+    const [gameEnded, setGameEnded] = useState(false);
+    const [gameStateRecorded, setGameStateRecorded] = useState(false); // New state to track if game state is recorded
 
     // Function to fetch a random meme from the API
-
     const fetchRandomMeme = async () => {
         try {
             let memeId;
@@ -49,7 +51,6 @@ const UserGame = () => {
     };
 
     // Function to fetch captions from the API
-
     const fetchCaptions = async () => {
         try {
             const response = await fetch('http://localhost:3001/api/caption');
@@ -66,7 +67,6 @@ const UserGame = () => {
     };
 
     // Function to fetch correct caption parts for a given meme ID
-
     const fetchCorrectCaption = async (memeId) => {
         try {
             const response = await fetch(`http://localhost:3001/api/correct-caption/${memeId}`);
@@ -85,7 +85,6 @@ const UserGame = () => {
     };
 
     // Function to initialize a new round
-
     const initializeRound = async () => {
         const memeId = await fetchRandomMeme();
         const captions = await fetchCaptions();
@@ -100,47 +99,45 @@ const UserGame = () => {
     };
 
     // Function to shuffle an array of all captions
-
     const shuffleArray = (array) => {
         return array.sort(() => Math.random() - 0.5);
     };
 
     // Effect to initialize the first round on component mount or round change
-
     useEffect(() => {
         initializeRound();
     }, [round]);
 
     // Effect to handle the game timer
-
     useEffect(() => {
-        const timerInterval = setInterval(() => {
-            setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : prevTimer));
-        }, 1000);
+        let timerInterval;
+        if (!gameEnded) {
+            timerInterval = setInterval(() => {
+                setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : prevTimer));
+            }, 1000);
+        }
 
         return () => clearInterval(timerInterval);
-    }, []);
+    }, [gameEnded]);
 
     // Effect to handle timer expiration
-
     useEffect(() => {
-        if (timer === 0) {
+        if (timer === 0 && !gameEnded) {
             handleTimerExpiration();
         }
-    }, [timer]);
+    }, [timer, gameEnded]);
 
     // Function to handle timer expiration
-
     const handleTimerExpiration = () => {
         setMessage("Time's up! You did not select a caption.");
         setMessageType('danger');
+        updateGameSummary(null, false);
         setTimeout(() => {
             nextRound();
         }, 2000);
     };
 
     // Function to handle caption click
-
     const handleCaptionClick = (caption) => {
         setSelectedCaption(caption);
         setCaptionClicks((prevClicks) => prevClicks + 1);
@@ -153,26 +150,41 @@ const UserGame = () => {
             setMessage('Correct! You got it right and 5 points added to your score!');
             setMessageType('success');
         } else {
-            setMessage(`Incorrect! You selected: "${caption}". The correct caption was: "${correctCaptionParts.join(' and ')}"`);
+            setMessage(`Incorrect! The correct caption was: "${correctCaptionParts.join(' <AND> ')}"`);
             setMessageType('danger');
         }
 
-        recordGameState(isCorrect);
+        updateGameSummary(caption, isCorrect);
 
-        if (round < 3) {
-            setTimeout(() => {
-                nextRound();
-            }, 2000);
-        } else {
-            setTimeout(() => {
-                setMessage(`Game Over! Your final score is ${score + (isCorrect ? 5 : 0)}.`);
-                setMessageType('info');
-            }, 2000);
+        // Record game state only if the user has completed the round
+        if (round === 3) {
+            recordGameState(isCorrect);
         }
+
+        setTimeout(() => {
+            if (round < 3 && !gameEnded) {
+                nextRound();
+            } else {
+                endGame();
+            }
+        }, 2000);
+    };
+
+    // Function to update game summary
+    const updateGameSummary = (caption, isCorrect) => {
+        setGameSummary((prevSummary) => [
+            ...prevSummary,
+            {
+                round,
+                meme: meme.image_path,
+                selectedCaption: caption,
+                isCorrect,
+                correctCaption: correctCaptionParts.join(' <AND> ')
+            }
+        ]);
     };
 
     // Function to record game state
-
     const recordGameState = async (isCorrect) => {
         try {
             const response = await fetch('http://localhost:3001/api/record-game', {
@@ -183,15 +195,16 @@ const UserGame = () => {
                 body: JSON.stringify({
                     round,
                     username,
-                    score: isCorrect ? 5 : 0, 
+                    score: isCorrect ? 5 : 0,
                     caption: selectedCaption || (correctCaptionParts.length > 0 ? correctCaptionParts[0] : null),
-                    rightcaption: correctCaptionParts.join(' and '),
+                    rightcaption: correctCaptionParts.join(' <AND> '),
                     image_path: meme.image_path // Include the image path in the request
                 }),
             });
             if (!response.ok) {
                 throw new Error('Failed to record game state');
             }
+            setGameStateRecorded(true); // Mark game state as recorded
         } catch (error) {
             console.error('Error recording game state:', error);
             setMessage('An error occurred while recording the game state.');
@@ -206,6 +219,23 @@ const UserGame = () => {
             setTimer(30);
             setMessage('Getting ready for the next round...');
             setMessageType('info');
+            setSelectedCaption(null);
+            setIsCorrectCaption(null);
+            setGameStateRecorded(false); // Reset game state recorded flag for next round
+        } else {
+            setGameEnded(true);
+        }
+    };
+
+    // Function to end the game
+    const endGame = () => {
+        setCaptionClicks(3);
+        setGameEnded(true);
+
+        // If the game has not been completed (user exits before round 3), ensure game state is not recorded
+        if (!gameStateRecorded) {
+            setGameSummary([]); // Clear game summary
+            setMessage('Game is not completed due to User exited before round 3 or the timer expired.');
         }
     };
 
@@ -216,11 +246,13 @@ const UserGame = () => {
         setUsedMemes([]);
         setMessage('');
         setMessageType('info');
+        setCaptionClicks(0);
+        setGameSummary([]);
+        setGameEnded(false);
         initializeRound();
     };
 
     // Function to get caption class based on correctness
-    
     const getCaptionClass = (caption) => {
         if (!selectedCaption) return '';
 
@@ -231,53 +263,71 @@ const UserGame = () => {
         return '';
     };
 
+
     return (
         <Container className="game-page-container">
-            <Row className="justify-content-center">
-                <Col md={8} className="text-center">
-                    <h2 className="mb-4">Round {round}</h2>
-                    <p className="mt-4">Time left: {timer} seconds</p>
-                    {message && <Alert variant={messageType} className="message">{message}</Alert>}
-                    {meme ? (
-                        <>
-                            <div className="meme-container">
-                                <img src={meme.image_path} alt="Meme" className="img-fluid rounded mb-4" />
-                            </div>
-                            <div className="caption-options">
-                                <h3>Select a caption:</h3>
-                                {captions.map((caption, index) => (
-                                    <div key={index}>
-                                        <Form.Check
-                                            type="radio"
-                                            id={`caption${index}`}
-                                            label={caption}
-                                            name="caption"
-                                            value={caption}
-                                            onClick={() => handleCaptionClick(caption)}
-                                            className={`caption-option ${getCaptionClass(caption)}`}
-                                        />
-                                    </div>
-                                ))}
-                                {round < 3 && (
-                                    <Button variant="primary" className="mt-3" onClick={nextRound}>Skip</Button>
-                                )}
-                                <Button variant="secondary" className="mt-3" onClick={() => navigate('/')}>Exit</Button>
-                            </div>
-                        </>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                    <p className="mt-4">Time left: {timer} seconds</p>
-                </Col>
-            </Row>
-            {captionClicks === 3 && (
+            {gameEnded ? (
                 <Row className="justify-content-center mt-4">
-                    <Col md={6} className="text-center">
+                    <Col md={8} className="text-center">
+                        <h2>Game Summary</h2>
                         <Alert variant="info">
                             <p>Game Over! Your final score is {score}.</p>
+                            <p>Game is not completed due to User exited before round 3 or the timer expired.</p>
+                            {gameSummary.map((roundSummary, index) => (
+                                <div key={index} className="round-summary mb-3">
+                                    <h4>Round {roundSummary.round}</h4>
+                                    <img src={roundSummary.meme} alt={`Meme Round ${roundSummary.round}`} className="img-fluid rounded mb-2" />
+                                    <p><strong>Selected Caption:</strong> {roundSummary.selectedCaption}</p>
+                                    {roundSummary.isCorrect ? (
+                                        <p className="text-success"><strong>Result:</strong> Correct</p>
+                                    ) : (
+                                        <>
+                                            <p className="text-danger"><strong>Result:</strong> Incorrect</p>
+                                            <p><strong>Correct Caption:</strong> {roundSummary.correctCaption}</p>
+                                        </>
+                                    )}
+                                </div>
+                            ))}
                             <Button variant="primary" className="mr-3" onClick={handlePlayAgain}>Play Again</Button>
                             <Button variant="secondary" onClick={() => navigate('/profile')}>Profile</Button>
                         </Alert>
+                    </Col>
+                </Row>
+            ) : (
+                <Row className="justify-content-center">
+                    <Col md={8} className="text-center">
+                        <h2 className="mb-4">Round {round}</h2>
+                        {message && <Alert variant={messageType} className="message">{message}</Alert>}
+                        {meme ? (
+                            <>
+                                <div className="meme-container">
+                                    <img src={meme.image_path} alt="Meme" className="img-fluid rounded mb-4" />
+                                </div>
+                                <div className="caption-options">
+                                    <h3>Select a caption:</h3>
+                                    {captions.map((caption, index) => (
+                                        <div key={index}>
+                                            <Form.Check
+                                                type="radio"
+                                                id={`caption${index}`}
+                                                label={caption}
+                                                name="caption"
+                                                value={caption}
+                                                onClick={() => handleCaptionClick(caption)}
+                                                className={`caption-option ${getCaptionClass(caption)}`}
+                                            />
+                                        </div>
+                                    ))}
+                                    {round < 3 && (
+                                        <Button variant="primary" className="mt-3" onClick={nextRound}>Skip</Button>
+                                    )}
+                                    <Button variant="secondary" className="mt-3" onClick={() => navigate('/')}>Exit</Button>
+                                </div>
+                            </>
+                        ) : (
+                            <p>Loading...</p>
+                        )}
+                        <p className="mt-4">Time left: {timer} seconds</p>
                     </Col>
                 </Row>
             )}
